2c165d5c76788b90401069d111f7de4bf9fb79e3
Exion <harashnabin2002@gmail.com>
2022-12-24 20:00:10 +0530

    InputField Finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React, {useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import {Todo} from './model';
+import TodoList from "./components/TodoList";
+
 
 const App: React.FC = () => {
 
   const[todo, setTodo] = useState<string>("");
+  const[todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
       <span className='heading'>
         Taskify
       </span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))} */}
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
