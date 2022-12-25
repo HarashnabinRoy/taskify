07d8b31e0cb84e59dbb2feff5ffb25f7cbb0eeb1
Exion <harashnabin2002@gmail.com>
2022-12-25 23:20:53 +0530

    Cards Added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Todo} from '../model';
 import './styles.css' 
+import SingleTodo from './SingleTodo';
 
 interface Props {
     todos: Todo[];
@@ -9,7 +10,14 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
   return <div className='todos'>
-    {}
+    {todos.map((todo) => (
+        <SingleTodo 
+            todo={todo} 
+            key={todo.id} 
+            todos={todos} 
+            setTodos={setTodos} 
+        />
+    ))}
   </div>
 }
 
