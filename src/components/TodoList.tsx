6a74653228c6382504350b564dfc624325a005d8
Exion <harashnabin2002@gmail.com>
2022-12-26 23:28:21 +0530

    Edit UI

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,18 +7,55 @@ interface Props {
     todos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-
 const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
-  return <div className='todos'>
-    {todos.map((todo) => (
-        <SingleTodo 
-            todo={todo} 
-            key={todo.id} 
-            todos={todos} 
-            setTodos={setTodos} 
-        />
-    ))}
-  </div>
+    return (
+        <div className='container'>
+            <div className='todos'>
+                <span className='todos_heading'>
+                    Active Task
+                </span>
+                {
+                    todos.map((todo) => (
+                        <SingleTodo 
+                            todo={todo}
+                            todos={todos}
+                            key={todo.id}
+                            setTodos={setTodos}
+                        />
+                    ))
+                }
+            </div>
+            <div className='todos remove'>
+                <span className='todos_heading'>
+                        Completed Task
+                </span>
+                {
+                    todos.map((todo) => (
+                        <SingleTodo 
+                            todo={todo}
+                            todos={todos}
+                            key={todo.id}
+                            setTodos={setTodos}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
+
+//-------------- OLD UI --------------
+// const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
+//   return <div className='todos'>
+//     {todos.map((todo) => (
+//         <SingleTodo 
+//             todo={todo} 
+//             key={todo.id} 
+//             todos={todos} 
+//             setTodos={setTodos} 
+//         />
+//     ))}
+//   </div>
+// }
 
 export default TodoList;
